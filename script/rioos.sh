ce68a2f69998ef91f746fba17cab5a66d7b0e25b
rajesh_rajagopal <rajeshr@megam.io>
2017-11-21 10:54:10 +0530

    script changes

--- script/rioos.sh
+++ script/rioos.sh
@@ -4,8 +4,7 @@
 dir=/var/lib
 sudo apt-get -y update
 sudo apt-get install -y software-properties-common python-software-properties
-sudo mkdir -p /var/lib/rioos/containers/certs
-sudo mkdir -p /var/lib/rioos/containers/auth
+mkdir -p /etc/docker/certs.d/registry.megam.io:5000
 
 # Install docker.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -18,27 +17,27 @@ sudo usermod -aG docker ${USER}
 sudo systemctl start docker
 sudo systemctl status docker
 
-# Create registry.megam.io keyfile.
-openssl req \
-  -newkey rsa:4096 -nodes -sha256 -keyout /var/lib/rioos/containers/certs/registry.megam.io.key \
-  -x509 -days 365 -out /var/lib/rioos/containers/certs/registry.megam.io.crt \
-  -subj '/CN=www.megam.io/O=Megam Systems/C=AU'
+# Get certificate keys.
+sudo wget https://s3.amazonaws.com/rioos/registry/ca.crt
+export CA_CRT=$PWD/ca.crt
+mv $CA_CRT /etc/docker/certs.d/registry.megam.io:5000
 
 # Login into registry.megam.io.
-docker login registry.megam.io:5000 -u rioosadmin -p team4rio
+sudo docker login registry.megam.io:5000 -u rioosadmin -p team4rio
 
 # Pull images from registry.
-docker pull registry.megam.io:5000/rioosccmock
+sudo docker pull registry.megam.io:5000/rioosccmock
 
-# Launch a container
+# Launch a docker container.
 CID=$(sudo docker run -d -it -p 4201:4201 --name=rioosccmock registry.megam.io:5000/rioosccmock)
 
 # Get container IP address.
-CIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CID)
+CIP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CID)
 
 # Rio/OS Configuration
 sed -i 's/\<quiet splash\>//g' /etc/default/grub
-sed -i 's/Ubuntu 16.04/Rio\/OS/g' /usr/share/plymouth/themes/ubuntu-text/ubuntu-text.plymouth
+sed -i 's/Ubuntu 16.04/Rio\/OS v2/g' /usr/share/plymouth/themes/ubuntu-text/ubuntu-text.plymouth
 
 # Update grub configuration
 update-grub
+
